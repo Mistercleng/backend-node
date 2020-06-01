f0f42f8ecd410819e0bc84b01462fee03ff7c1ee
Mistercleng <mrcleng@gmail.com>
2020-05-31 23:45:12 -0400

    backend node save on database and use multer

--- jest.config.js
+++ jest.config.js
@@ -66,14 +66,14 @@ module.exports = {
   // ],
 
   // An array of file extensions your modules use
-  // moduleFileExtensions: [
-  //   "js",
+   moduleFileExtensions: [
+     "js",
   //   "json",
   //   "jsx",
-  //   "ts",
+     "ts",
   //   "tsx",
   //   "node"
-  // ],
+  ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   // moduleNameMapper: {},
